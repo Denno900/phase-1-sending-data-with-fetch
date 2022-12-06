c5888587939b91f980d4de7655b03d9016f64f0d
dennis mwaniki <dennomaz@gmail.com>
2022-12-06 12:50:44 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Add your code here
+
+function submitData(userName, userEmail){
+    const user = {
+        "name":`${userName}`,
+        "email":`${userEmail}`
+    }
+    return fetch('http://localhost:3000/users/1', {
+        method: "PUT",
+        headers: {
+            "Content-type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify(user)
+
+    })
+       .then(response => response.json())
+       .then(data => document.body.innerHTML = data["id"])
+       .catch(error => document.body.innerHTML = error.message)
+
+}   
+
+console.log(submitData("Dennis", "denno01@gmail.com"))
